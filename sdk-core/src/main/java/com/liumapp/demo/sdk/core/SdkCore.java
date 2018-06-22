d0c4c0729486a8ca6ac7f4ce85e4fd3f6ca56aec
liumapp <liumapp.com@gmail.com>
2018-06-22 15:38:06 +0800

    rm unnecessary files in sdk-core

--- sdk-core/src/main/java/com/liumapp/demo/sdk/core/SdkCore.java
+++ sdk-core/src/main/java/com/liumapp/demo/sdk/core/SdkCore.java
@@ -1,8 +1,5 @@
 package com.liumapp.demo.sdk.core;
 
-import com.liumapp.demo.sdk.core.config.UtilConfig;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 /**
  * @author liumapp
@@ -11,9 +8,8 @@ import org.springframework.context.annotation.Import;
  * @homepage http://www.liumapp.com
  * @date 6/22/18
  */
-@Configuration
-@Import({UtilConfig.class})
 public class SdkCore {
+
 
 
 }
