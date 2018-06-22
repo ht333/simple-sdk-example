b26ac6c9d6f65f6df6f05161ec6fc65ab376efd4
liumapp <liumapp.com@gmail.com>
2018-06-22 14:22:11 +0800

    add backup controller

--- sdk-server/src/main/java/com/liumapp/demo/sdk/client/conf/DruidDatasourceConfig.java
+++ sdk-server/src/main/java/com/liumapp/demo/sdk/client/conf/DruidDatasourceConfig.java
@@ -30,5 +30,4 @@ public class DruidDatasourceConfig {
         return DruidDataSourceBuilder.create().build();
     }
 
-
 }
---
+++ sdk-server/src/main/java/com/liumapp/demo/sdk/client/controller/BackupController.java
@@ -0,0 +1,19 @@
+package com.liumapp.demo.sdk.client.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liumapp
+ * @file BackupController.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/22/18
+ */
+@RestController
+@RequestMapping("backup")
+public class BackupController {
+
+    
+
+}
