75e91fefdad3e0c3be9f4d6c1170fd161d571306
liumapp <liumapp.com@gmail.com>
2018-06-22 17:18:33 +0800

    add file back up item

---
+++ sdk-core/src/main/java/com/liumapp/demo/sdk/core/backup/AddFileBackUpItem.java
@@ -0,0 +1,20 @@
+package com.liumapp.demo.sdk.core.backup;
+
+import com.liumapp.demo.sdk.core.JobData;
+import com.liumapp.demo.sdk.core.JobDetail;
+import com.liumapp.demo.sdk.core.file.FileSaveAble;
+
+/**
+ * @author liumapp
+ * @file AddFileBackUpItem.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/22/18
+ */
+public class AddFileBackUpItem extends JobDetail implements FileSaveAble {
+
+    public String handle(JobData data) {
+        return null;
+    }
+
+}
