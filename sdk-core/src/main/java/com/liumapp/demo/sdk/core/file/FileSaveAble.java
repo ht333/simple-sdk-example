f1f3260ffca2a16af2dca6c3bf7bcc7c8317f16e
liumapp <liumapp.com@gmail.com>
2018-06-22 17:17:05 +0800

    add file save able

---
+++ sdk-core/src/main/java/com/liumapp/demo/sdk/core/file/FileSaveAble.java
@@ -0,0 +1,14 @@
+package com.liumapp.demo.sdk.core.file;
+
+/**
+ * @author liumapp
+ * @file FileSaveAble.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/22/18
+ */
+public interface FileSaveAble {
+
+
+
+}
