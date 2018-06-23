e0a37402f209849737bc68465f5822916690f738
liumapp <liumapp.com@gmail.com>
2018-06-23 11:26:56 +0800

    add annotation

--- sdk-core/src/main/java/com/liumapp/demo/sdk/core/backup/GetBackUpItem.java
+++ sdk-core/src/main/java/com/liumapp/demo/sdk/core/backup/GetBackUpItem.java
@@ -3,6 +3,13 @@ package com.liumapp.demo.sdk.core.backup;
 import com.liumapp.demo.sdk.core.JobDetail;
 import com.liumapp.demo.sdk.core.backup.require.GetBackUpItemRequire;
 
+/**
+ * @author liumapp
+ * @file GetBackUpItem.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/23/18
+ */
 public class GetBackUpItem extends JobDetail<GetBackUpItemRequire> {
 
     public String handle(GetBackUpItemRequire data) {
--- sdk-core/src/main/java/com/liumapp/demo/sdk/core/backup/require/GetBackUpItemRequire.java
+++ sdk-core/src/main/java/com/liumapp/demo/sdk/core/backup/require/GetBackUpItemRequire.java
@@ -2,5 +2,12 @@ package com.liumapp.demo.sdk.core.backup.require;
 
 import com.liumapp.demo.sdk.core.JobData;
 
+/**
+ * @author liumapp
+ * @file GetBackUpItemRequire.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/23/18
+ */
 public class GetBackUpItemRequire extends JobData {
 }
