217f95883a1957cf2ea13103f2ae25bc7f16e41f
liumapp <liumapp.com@gmail.com>
2018-06-23 14:06:00 +0800

    add backup-info component

---
+++ client-ui/src/components/backup-info.vue
@@ -0,0 +1,15 @@
+/**
+ * @file backup-info.vue
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/23/18
+ */
+<template>
+</template>
+<script>
+export default {
+  name: 'backup-info',
+  
+}
+</script>
