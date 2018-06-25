18533c6ec19eda4e2b34d42e913f41eb8e3bc2e1
liumapp <liumapp.com@gmail.com>
2018-06-25 15:49:35 +0800

    ready to use in docker

--- sdk-core/src/main/java/com/liumapp/demo/sdk/core/conf/HostConfig.java
+++ sdk-core/src/main/java/com/liumapp/demo/sdk/core/conf/HostConfig.java
@@ -2,7 +2,7 @@ package com.liumapp.demo.sdk.core.conf;
 
 public class HostConfig {
 
-    public static String host = "http://localhost:3030/";
+    public static String host = "http://sdk-server:3030/";
 
     public static String addBackUpItemPath = "backup/add";
 
