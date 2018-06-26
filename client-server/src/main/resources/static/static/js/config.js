ed8eec58523a43b20a67f6800978cbbfa3ffaf90
liumapp <liumapp.com@gmail.com>
2018-06-26 14:54:59 +0800

    ready to test liumapp.com

---
+++ client-server/src/main/resources/static/static/js/config.js
@@ -0,0 +1,11 @@
+/**
+ * @file config.js
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/26/18
+ */
+
+window.globalconf={
+  SERVICE_CONTEXT_PATH:`http://www.liumapp.com:2020/`//配置服务器地址
+}
